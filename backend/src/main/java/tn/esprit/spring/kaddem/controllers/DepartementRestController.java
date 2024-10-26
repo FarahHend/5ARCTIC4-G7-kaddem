9f23bb9ddfc381b4c799f942c55bff1503841e8c
baha8brinsi <mohamedbahaeddine.brinsi@esprit.tn>
2024-10-26 16:49:14 +0100

    Ajout de la journalisation (Log4j) pour les méthodes de gestion des départements

--- backend/src/main/java/tn/esprit/spring/kaddem/controllers/DepartementRestController.java
+++ backend/src/main/java/tn/esprit/spring/kaddem/controllers/DepartementRestController.java
@@ -1,47 +1,91 @@
 package tn.esprit.spring.kaddem.controllers;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.kaddem.entities.Departement;
 import tn.esprit.spring.kaddem.services.IDepartementService;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("/departement")
 public class DepartementRestController {
+	private static final Logger logger = LogManager.getLogger(DepartementRestController.class);
+
 	IDepartementService departementService;
 	// http://localhost:8089/Kaddem/departement/retrieve-all-departements
 	@GetMapping("/retrieve-all-departements")
-	public List<Departement> getDepartements() {
-		List<Departement> listDepartements = departementService.retrieveAllDepartements();
-		return listDepartements;
+	public ResponseEntity<List<Departement>> getDepartements() {
+		logger.info("Requête GET reçue pour récupérer tous les départements");
+		try {
+			List<Departement> departements = departementService.retrieveAllDepartements();
+			logger.info("Nombre de départements récupérés : {}", departements.size());
+			return ResponseEntity.ok(departements);
+		} catch (Exception e) {
+			logger.error("Erreur lors de la récupération des départements", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 	// http://localhost:8089/Kaddem/departement/retrieve-departement/8
 	@GetMapping("/retrieve-departement/{departement-id}")
-	public Departement retrieveDepartement(@PathVariable("departement-id") Integer departementId) {
-		return departementService.retrieveDepartement(departementId);
+	public ResponseEntity<Departement> retrieveDepartement(@PathVariable("departement-id") Integer departementId) {
+		logger.info("Requête GET reçue pour récupérer le département avec ID : {}", departementId);
+		try {
+			Departement departement = departementService.retrieveDepartement(departementId);
+			logger.info("Département récupéré avec succès : {}", departement.getNomDepart());
+			return ResponseEntity.ok(departement);
+		} catch (Exception e) {
+			logger.error("Erreur lors de la récupération du département avec ID : {}", departementId, e);
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
 	}
 
 	// http://localhost:8089/Kaddem/departement/add-departement
 	@PostMapping("/add-departement")
-	public Departement addDepartement(@RequestBody Departement d) {
-		Departement departement = departementService.addDepartement(d);
-		return departement;
+	public ResponseEntity<Departement> addDepartement(@RequestBody Departement d) {
+		logger.info("Requête POST reçue pour ajouter un département : {}", d.getNomDepart());
+		try {
+			Departement newDepartement = departementService.addDepartement(d);
+			logger.info("Département ajouté avec succès : {}", newDepartement.getNomDepart());
+			return ResponseEntity.status(HttpStatus.CREATED).body(newDepartement);
+		} catch (Exception e) {
+			logger.error("Erreur lors de l'ajout du département : {}", d.getNomDepart(), e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 	// http://localhost:8089/Kaddem/departement/remove-departement/1
 	@DeleteMapping("/remove-departement/{departement-id}")
-	public void removeDepartement(@PathVariable("departement-id") Integer departementId) {
-		departementService.deleteDepartement(departementId);
+	public ResponseEntity<Void> removeDepartement(@PathVariable("departement-id") Integer departementId) {
+		logger.warn("Requête DELETE reçue pour supprimer le département avec ID : {}", departementId);
+		try {
+			departementService.deleteDepartement(departementId);
+			logger.info("Département supprimé avec succès : ID {}", departementId);
+			return ResponseEntity.noContent().build();
+		} catch (Exception e) {
+			logger.error("Erreur lors de la suppression du département avec ID : {}", departementId, e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 	// http://localhost:8089/Kaddem/departement/update-departement
 	@PutMapping("/update-departement")
-	public Departement updateDepartement(@RequestBody Departement e) {
-		Departement departement= departementService.updateDepartement(e);
-		return departement;
+	public ResponseEntity<Departement> updateDepartement(@RequestBody Departement d) {
+		logger.info("Requête PUT reçue pour mettre à jour le département avec ID : {}", d.getIdDepart());
+		try {
+			Departement updatedDepartement = departementService.updateDepartement(d);
+			logger.info("Département mis à jour avec succès : {}", updatedDepartement.getNomDepart());
+			return ResponseEntity.ok(updatedDepartement);
+		} catch (Exception e) {
+			logger.error("Erreur lors de la mise à jour du département avec ID : {}", d.getIdDepart(), e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 }
 
--- backend/src/main/java/tn/esprit/spring/kaddem/services/DepartementServiceImpl.java
+++ backend/src/main/java/tn/esprit/spring/kaddem/services/DepartementServiceImpl.java
@@ -10,33 +10,79 @@ import tn.esprit.spring.kaddem.repositories.ContratRepository;
 import tn.esprit.spring.kaddem.repositories.DepartementRepository;
 
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 @Slf4j
 
 @Service
 public class DepartementServiceImpl implements IDepartementService{
+	private static final Logger logger = LogManager.getLogger(DepartementServiceImpl.class);
+
 	@Autowired
 	DepartementRepository departementRepository;
 	public List<Departement> retrieveAllDepartements(){
-		return (List<Departement>) departementRepository.findAll();
+
+		logger.info("Début de la récupération de tous les départements depuis la base de données.");
+		try {
+			List<Departement> departements = (List<Departement>) departementRepository.findAll();
+			logger.info("Récupération réussie de tous les départements. Nombre total : {}", departements.size());
+			return departements;
+		} catch (Exception e) {
+			logger.error("Erreur lors de la récupération des départements depuis la base de données", e);
+			throw e;
+		}
 	}
 
 	public Departement addDepartement (Departement d){
-		return departementRepository.save(d);
+
+		logger.info("Début de l'ajout d'un nouveau département : {}", d.getNomDepart());
+		try {
+			Departement savedDepartement = departementRepository.save(d);
+			logger.info("Département sauvegardé dans la base de données avec succès : {}", savedDepartement.getNomDepart());
+			return savedDepartement;
+		} catch (Exception e) {
+			logger.error("Erreur lors de la sauvegarde du département dans la base de données : {}", d.getNomDepart(), e);
+			throw e;
+		}
 	}
 
 	public   Departement updateDepartement (Departement d){
-		return departementRepository.save(d);
+		logger.info("Début de la mise à jour du département avec ID : {}", d.getIdDepart());
+		try {
+			Departement updatedDepartement = departementRepository.save(d);
+			logger.info("Département sauvegardé avec succès dans la base de données : {}", updatedDepartement.getNomDepart());
+			return updatedDepartement;
+		} catch (Exception e) {
+			logger.error("Erreur lors de la mise à jour du département avec ID : {}", d.getNomDepart(), e);
+			throw e;
+		}
 	}
 
 	public  Departement retrieveDepartement (Integer idDepart){
-		return departementRepository.findById(idDepart).get();
+
+		logger.info("Récupération du département avec ID : {}", idDepart);
+		try {
+			Departement departement = departementRepository.findById(idDepart)
+					.orElseThrow(() -> new RuntimeException("Département non trouvé : ID " + idDepart));
+			logger.info("Département récupéré avec succès : {}", departement.getNomDepart());
+			return departement;
+		} catch (Exception e) {
+			logger.error("Erreur lors de la récupération du département avec ID : {}", idDepart, e);
+			throw e;
+		}
 	}
+
 	public  void deleteDepartement(Integer idDepartement){
-		Departement d=retrieveDepartement(idDepartement);
-		departementRepository.delete(d);
+		logger.warn("Suppression du département avec ID : {}", idDepartement);
+		try {
+			departementRepository.deleteById(idDepartement);
+			logger.info("Département supprimé avec succès : ID {}", idDepartement);
+		} catch (Exception e) {
+			logger.error("Erreur lors de la suppression du département avec ID : {}", idDepartement, e);
+			throw e;
+		}
 	}
-
-
 
 }

007084a7b4e4ce2e10d3ff3b4c1a11374f783a10
Marayed <marayed0@gmail.com>
2024-11-04 23:47:29 +0100

    cors allow all

--- src/main/java/tn/esprit/spring/kaddem/config/WebConfig.java
+++ src/main/java/tn/esprit/spring/kaddem/config/WebConfig.java
@@ -1,5 +1,7 @@
 package tn.esprit.spring.kaddem.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.33.10:4200")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+        logger.info("CORS configuration applied with allowed origin: *");
     }
 }

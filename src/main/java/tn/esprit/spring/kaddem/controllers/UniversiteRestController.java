9a2bf17a0f82291cc9c025c0e8bd8fd9022bd3d5
Marayed <marayed0@gmail.com>
2024-11-04 23:35:54 +0100

    origin fix

--- src/main/java/tn/esprit/spring/kaddem/controllers/UniversiteRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/UniversiteRestController.java
@@ -10,6 +10,7 @@ import tn.esprit.spring.kaddem.services.IUniversiteService;
 import java.util.List;
 import java.util.Set;
 
+@CrossOrigin(origins = "http://192.168.33.10:4200")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/universite")
